6236ef20a7401375641c4d13f0c79b3b13bb0c1d
Hitesh Purshotam Dayaramani <hdayaram@tibco.com>
2022-06-23 18:45:10 +0530

    Completed Misc-5

---
+++ RemoveAllAdjacentDuplicatesInStringII.java
@@ -0,0 +1,28 @@
+//Time Complexity O(N)
+//Space ComplexityO(N)
+//LeetCode tested
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInStringII {
+    public String removeDuplicates(String s, int k) {
+        Stack<int[]> main = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if(!main.isEmpty() && main.peek()[0] == c){
+                main.peek()[1]++;
+            }else main.push(new int[]{c,1});
+
+            if(main.peek()[1] == k){
+                main.pop();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!main.isEmpty()){
+            int[] top = main.pop();
+            while (top[1]-- >0){
+                sb.append((char)top[0]);
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
